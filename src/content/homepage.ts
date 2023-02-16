a8451958a6067def1a90938ccdd27c2562e8c825
gvdr <giulio.dallariva@canterbury.ac.nz>
2023-02-17 11:58:48 +1300

    draft 1'

---
+++ src/content/homepage.ts
@@ -0,0 +1,17 @@
+export const homepage = [
+  {
+    title: "Talks",
+    content: "Nice friends will tell us all about complexity.",
+    icon: "charm:rocket",
+  },
+  {
+    title: "Discussions",
+    content: "We will have a lot of lovely chats.",
+    icon: "ph:code-bold",
+  },
+  {
+    title: "Tutorials",
+    content: "We may even learn something together.",
+    icon: "charm:plant-pot",
+  },
+];
